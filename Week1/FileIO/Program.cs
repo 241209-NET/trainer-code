deccdb74deacc19375903cd85dc5fa7f65982bc6
Kung Lo <kungyang.lo@revature.com>
2024-12-11 11:22:50 -0500

    File IO

---
+++ Week1/FileIO/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace FileIO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filename = "example.txt";
+
+        Console.WriteLine(WriteFile(filename, "Hello, world", false));
+        
+        Console.WriteLine(ReadFile(filename));
+
+        string s = "This is some more text";
+
+        Console.WriteLine(WriteFile(filename, s, true));
+
+        Console.WriteLine(ReadFile(filename));
+    }
+
+    public static string ReadFile(string filename)
+    {
+        using(StreamReader sr = new(filename))
+        {
+            return sr.ReadToEnd();
+        }        
+    }
+
+    public static string WriteFile(string filename, string text, bool doAppend)
+    {
+        using(StreamWriter sw = new(filename, append: doAppend))
+        {
+            sw.WriteLine(text);
+        }
+
+        return "Write successful.";
+    }
+}
