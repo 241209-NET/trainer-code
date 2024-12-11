47e1086791d75073c45776d55bf04ca346a2b84e
Kung Lo <kungyang.lo@revature.com>
2024-12-11 13:39:18 -0500

    Classes

--- Week1/Classes/Program.cs
+++ Week1/Classes/Program.cs
@@ -1,29 +1,45 @@
-﻿namespace Classes;
+﻿using Movies;
+
+namespace Classes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Console.WriteLine("Hello, World!");
 
-        Car myCar1 = new("Toyota");
-        Car myCar2 = new("Toyota", "Camry", "Red", 100.0);
+        // Car myCar1 = new("Toyota");
+        // Car myCar2 = new("Toyota", "Camry", "Red", 100.0);
 
-        Console.WriteLine(myCar1.Getmake());
-        Console.WriteLine(myCar1.Getmodel());
-        Console.WriteLine(myCar1.Getcolor());
-        Console.WriteLine(myCar1.Getspeed());
+        // Console.WriteLine(myCar1.Getmake());
+        // Console.WriteLine(myCar1.Getmodel());
+        // Console.WriteLine(myCar1.Getcolor());
+        // Console.WriteLine(myCar1.Getspeed());
 
-        Console.WriteLine(myCar2.Getmake());
-        Console.WriteLine(myCar2.Getmodel());
-        Console.WriteLine(myCar2.Getcolor());
-        Console.WriteLine(myCar2.Getspeed());
+        // Console.WriteLine(myCar2.Getmake());
+        // Console.WriteLine(myCar2.Getmodel());
+        // Console.WriteLine(myCar2.Getcolor());
+        // Console.WriteLine(myCar2.Getspeed());
+
+        Movie myMov = new();
+        myMov.Name = "Titanic";
+        Console.WriteLine(myMov.Name);
+
+        Movie myMov2 = new(){ Name = "Cars2", Release = "2010", Copies = 50, Price = 350};
+
+        Console.WriteLine(myMov2.Name);
+        Console.WriteLine(myMov2.Release);
+        Console.WriteLine(myMov2.Copies);
+        Console.WriteLine(myMov2.Price);
+        
+        Console.WriteLine(Movie.FavMovie());
     }
 }
 
+//Creating a class in the same file
 public class Car
 {
-    //Attributes or fields
+    //Attributes or Fields
     private string _make = "Ferrari";
     private string _model = "F1";
     private string _color = "Null";
@@ -49,8 +65,8 @@ public class Car
         else _color = color;
     }
 
+    //Expression-bodied Methods
     public void Setspeed(double speed) => _speed = speed;
-
     public string Getmake() => _make;
     public string Getmodel() => _model;
     public string Getcolor() => _color;
