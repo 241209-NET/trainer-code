5a7d46fb86cb84e8e056a4185f90068fd8143c79
Kung Lo <kungyang.lo@revature.com>
2024-12-18 10:43:52 -0500

    coding challenge

---
+++ Week2/CodeChallengeReview/Palindrome/Program.cs
@@ -0,0 +1,63 @@
+﻿/*
+You are given a single string, and it is your job to figure out if that string is a palindrome. A palindrome is a number or word that are the same forwards and backwards (with exact upper/lower casing).
+
+The checkForPalindrome() method takes in 1 argument:
+s - the string that you are checking to see if it's a palindrome
+Once you've determined whether or not the string is a palindrome, return either true or false.
+
+
+                            EXPECTED OUTPUT
+Happy Birthday!	            false
+RACECAR	                    true
+amanaplanacanalpanama       true
+Saippuakivikauppias	        false
+*/
+
+namespace Palindrome;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(IsPalindrome3("Happy Birthday!") ? "true" : "false");
+        Console.WriteLine(IsPalindrome3("RACECAR") ? "true" : "false");
+        Console.WriteLine(IsPalindrome3("amanaplanacanalpanama") ? "true" : "false");
+        Console.WriteLine(IsPalindrome3("Saippuakivikauppias") ? "true" : "false");
+    }
+
+    public static bool IsPalindrome(string s)
+    {
+        //build a reverse of the string s and then check for equality
+        string revS = "";
+        for(int i = s.Length - 1; i >= 0; i--)
+        {
+            revS += s[i];
+        }
+
+        return revS.Equals(s);
+    }
+
+    public static bool IsPalindrome2(string s)
+    {
+        //Making new char array then reversing it
+        char[] cArr = s.ToCharArray();
+        Array.Reverse(cArr);
+        string c = new(cArr);
+        return c.Equals(s);
+    }
+
+    public static bool IsPalindrome3(string s)
+    {
+        //Check 1st and last character and move in each time.
+        int left = 0;
+        int right = s.Length - 1;
+
+        while(left < right)
+        {
+            if(s[left] != s[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
---
+++ Week2/CodeChallengeReview/SecretCode/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+You are tasked to convert a string into a secret code. Convert letters (ignoring case) and other characters into a secret code using the following rules:
+
+A = 1, B = 2, C = 3, D = 4, E = 5, F = 6, G = 7
+All other letters or characters are represented with a '?' in the secret code.
+
+The createSecretCode() method takes in 1 argument:
+s - the string that will be converted into the secret code string
+Once you've created the secret code version of the string, return the secret code string.
+
+
+                        EXPECTED OUTPUT
+FACE		            6135
+Coding	                3?4??7
+Feed		            6554
+This is a sentence	    ????????1??5??5?35
+Cab!		            312?
+*/
+
+namespace SecretCode;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(SecretCode3("FACE"));
+        Console.WriteLine(SecretCode3("Coding"));
+        Console.WriteLine(SecretCode3("Feed"));
+        Console.WriteLine(SecretCode3("This is a sentence"));
+        Console.WriteLine(SecretCode3("Cab!"));
+    }
+
+    static string SecretCode(string s)
+    {
+        //Check every character in s and add to code
+        string code = "";
+
+        s = s.ToUpper();
+
+        //if else chain
+        foreach(char c in s)
+        {
+            if(c == 'A') code += "1";
+            else if(c == 'B') code += "2";
+            else if(c == 'C') code += "3";
+            else if(c == 'D') code += "4";
+            else if(c == 'E') code += "5";
+            else if(c == 'F') code += "6";
+            else if(c == 'G') code += "7";
+            else code += "?";
+        }
+
+        return code;
+    }
+
+    static string SecretCode2(string s)
+    {
+        throw new NotImplementedException();
+        //See above except use switch statement
+
+        //Another way is to use a dictionary
+    }
+
+    static string SecretCode3(string s)
+    {
+        //Try using ASCII table        
+        string code = "";
+
+        s = s.ToUpper();
+
+        foreach(char c in s)
+        {
+            if(c >= 'A' && c <= 'G') code += c - 'A' + 1;
+            else code += '?';
+        }
+        return code;
+    }
+}
